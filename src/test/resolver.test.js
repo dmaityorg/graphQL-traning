6a0f9acad8e047096ec7a6a35346b861584f856d
Debasish Maity <dmaity@celestialsys.com>
2020-10-14 16:16:34 +0530

    added more test cases

--- src/test/resolver.test.js
+++ src/test/resolver.test.js
@@ -10,10 +10,8 @@ let expect = chai.expect;
 
 describe("Curd operations", function () {
   const requester = chai.request(app).keepOpen();
-  let dummyPost;
   before(async () => {
     dummyUser = await models.user.create({first_name: "debasish", last_name: "maity", email: "xxx", address: "xxx"});
-    console.log(dummyUser)
   });
 
   after(async () => {
@@ -28,11 +26,36 @@ describe("Curd operations", function () {
   describe("#Graphql", function () {
     it("Find user", function (done) {
       requester.post("/graphql")
-        .send({query: '{ getUser(id: '+ dummyUser.id +') { id first_name } }'})
+        .send({query: '{ getUser(id: '+ dummyUser.id +') { id first_name posts{ title } } }'})
         .end((err, res) => {
           if (err) return done(err);
+          console.log(res.body)
           res.body.data.getUser.should.have.property('id');
           res.body.data.getUser.should.have.property('first_name');
+          done();
+        });
+    });
+
+    it("Lists all users", function (done) {
+      requester.post("/graphql")
+        .send({ query: '{ getAllUsers { id first_name last_name posts { title } } }' })
+        .end((err, res) => {
+          if (err) return done(err);
+          res.body.data.getAllUsers[0].should.have.property("id");
+          res.body.data.getAllUsers[0].should.not.have.property("email");
+          res.body.data.getAllUsers[0].should.have.property("last_name");
+          done();
+        });
+    });
+
+    it("Create users", function (done) {
+      requester.post("/graphql")
+        .send({ query: 'mutation{ createUser(first_name: "Celestial", last_name: "system", email: "dmaity@gmail.com") { id first_name last_name } }' })
+        .end((err, res) => {
+          if (err) return done(err);
+          res.body.data.createUser.should.have.property("id");
+          res.body.data.createUser.should.not.have.property("email");
+          res.body.data.createUser.should.have.property("last_name");
           done();
         });
     });
